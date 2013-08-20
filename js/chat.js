8bfaff729446c4019dc66ffeda7cf7db00cda01c
Hack Reactor Students <github@hackreactor.com>
2013-08-19 23:31:29 -0700

    users are saved server-side and recognized when user logs in

--- js/chat.js
+++ js/chat.js
@@ -8,7 +8,11 @@ var getMessages = function(){
           if(data.results[i].username === undefined){
             data.results[i].username = "Guest";
           }
-          $('#main').append("<h2><a href='#' class='username'></h2>");
+          if(sessionStorage[data.results[i].username] === "true"){
+            $('#main').append("<h2 class='friend'><a href='#' class='username'></h2>");
+          } else {
+            $('#main').append("<h2><a href='#' class='username'></h2>");
+          }
           $('#main a:last').text(data.results[i].username);
           $('#main').append("<p class='message'>");
           $('#main p:last').text(data.results[i].text);
@@ -41,7 +45,7 @@ var createUser = function(username, password) {
   $.ajax('https://api.parse.com/1/users',{
     type: "POST",
     contentType: 'application/json',
-    data: JSON.stringify({username: username, password: password, friends: {} })
+    data: JSON.stringify({username: username, password: password, friendList: "{}" })
   });
 };
 
@@ -50,23 +54,29 @@ var loginUser = function(username, password) {
     type: "GET",
     contentType: 'application/json',
     success: function(data){
+      console.log(data);
+      // console.log(serverFriends);
+      for(var key in data.friendList){
+        // var sessionFriends = jQuery.parseJSON(sessionStorage);
+        console.log(key);
+        sessionStorage[key] = true;
+      }
       sessionStorage.authToken = data.sessionToken;
       sessionStorage.userId = data.objectId;
-      sessionStorage.friends = data.friends;
     }
   });
 };
 
 
 var addFriend = function(friendName) {
-  sessionStorage.friends[friendName] = true;
-  friends = sessionStorage.friends;
+  sessionStorage.setItem(friendName, true);
+  friends = sessionStorage;
+  console.log(typeof friends);
   console.log('https://api.parse.com/1/users/' + sessionStorage.userId);
   $.ajax('https://api.parse.com/1/users/' + sessionStorage.userId, {
     type: "PUT",
-    sessionToken: sessionStorage.authToken,
     contentType: 'application/json',
-    data: JSON.stringify({friends: friends})
+    data: JSON.stringify({friendList: friends})
   });
 };
 
@@ -74,12 +84,21 @@ $(document).ready(function(){
   $('body').on('click', '.username', function(event){
       event.preventDefault();
       var friendName = $(this).text();
+      console.log(friendName);
       $(this).css('color', 'green');
       console.log("yes");
       addFriend(friendName);
 
   });
 });
+
+setInterval(function(){
+  var colorArray = [0,1,2,3,4,5,6,7,8,9, "a", "b", "c", "d", "e", "f"];
+  var getRandomColor = function(colorArray){return colorArray[Math.floor(Math.random()*colorArray.length)]; };
+  var randomColors = "#" + getRandomColor(colorArray) + getRandomColor(colorArray) + getRandomColor(colorArray) + getRandomColor(colorArray) + getRandomColor(colorArray) + getRandomColor(colorArray);
+
+  $('.friend').css('background-color', randomColors);
+}, 1000);
 
 
 
